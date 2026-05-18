713db752e8f17eb50ef3e088bad5433c451aa4bb
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-05-18 14:05:02 +0530

    proxy only in dev

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,9 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { proxy } from './proxy';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [sveltekit()],
-  server: {
-    proxy
-  }
-});
+  ...(command === 'serve'
+    ? {
+        server: {
+          proxy
+          // proxy only during development
+        }
+      }
+    : {})
+}));
