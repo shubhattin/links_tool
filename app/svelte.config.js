8344de023c16463c73f9d909e54d3b7b91ae284c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-05-31 18:52:22 +0530

    restore static dapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      entries: ['/']
+    }
   }
 };
 
