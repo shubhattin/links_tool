67c77ac6abe438ed4a09f4def080a05120b67b7c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-07-14 13:38:38 +0530

    adapter static

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs22.x'
-    }),
+    adapter: adapter(),
     prerender: {
       entries: ['/']
     }
